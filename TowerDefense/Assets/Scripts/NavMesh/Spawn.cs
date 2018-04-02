3d8553eff88cbb360b151bd4bf241d6ce80d2a4c
GD110/Emanuel <emanuelsg95@gmail.com>
2018-04-02 03:59:23 -0700

    taanks

---
+++ TowerDefense/Assets/Scripts/NavMesh/Spawn.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour {
+
+    public GameObject nagent;
+
+	void Start ()
+    {
+        Invoke("SpawnAgent", 2);
+	}
+	
+    void SpawnAgent()
+    {
+        GameObject na = (GameObject)Instantiate(nagent, this.transform.position, Quaternion.identity);
+        Invoke("SpawnAgent", Random.Range(2, 5));
+    }
+
+}
