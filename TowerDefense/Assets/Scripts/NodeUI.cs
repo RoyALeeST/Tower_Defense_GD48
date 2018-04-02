b4102a93f29b1f76d842667abbc6fc076e70c977
RoyALeeST <monchicki@hotmail.com>
2018-04-02 01:46:31 -0700

    Upgrade/Sell
    Added Upgrade and selling functionality

--- TowerDefense/Assets/Scripts/NodeUI.cs
+++ TowerDefense/Assets/Scripts/NodeUI.cs
@@ -1,13 +1,28 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class NodeUI : MonoBehaviour {
 
 	private Node target;
 	public GameObject ui;
+	public Text upgradeCostText;
+	public Text sellAmountText;
+	public Button upgradeButton;
+
 
 	public void SetTarget(Node _target){
 		 target = _target;
 		 transform.position = target.GetBuildPosition();
+		 sellAmountText.text = "$" + target.turretBlueprint.GetSellAmount();
+		if(!target.isUpgraded){
+			
+			upgradeCostText.text = "$" + target.turretBlueprint.costOfTurret;
+			upgradeButton.interactable = true;
+		}else{
+			upgradeCostText.text = "Max lvl";
+			upgradeButton.interactable = false;
+		}
+
 		 ui.SetActive(true);
 	}
 
@@ -15,8 +30,17 @@ public class NodeUI : MonoBehaviour {
 		ui.SetActive(false);
 	}
 
+	public void Sell(){
+		target.sellTurret();
+		 //After selling we want to deselect all nodes that 
+		 //we have selected so the UI disappears
+		BuildManager._instance.DeselectNode();
+	}
+
 	public void Upgrade(){
 		target.UpgradeTurret();
+		//After selling we want to deselect all nodes that 
+		//we have selected so the UI disappears
 		BuildManager._instance.DeselectNode();
 	}
 }
